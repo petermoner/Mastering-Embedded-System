1fbe1005802aebbba836175b4b72b4c33e6b68a3
Peter Moner <petomony5@gmail.com>
2022-10-11 02:51:03 +0200

    Add Task3

---
+++ UNIT_2_C Programming/Codes/C_Basics/Assignment_1/Print_a_Sentence/main.c
@@ -0,0 +1,13 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 10, 2022
+ *      Author: Peter Moner
+ */
+#include <stdio.h>
+
+int main (void)
+{
+	printf("\#\#\#\#\#\#\#\#\#\#Console-output\#\#\#\nC Programming\n\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#");
+	return 0;
+}
